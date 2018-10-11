460b249a9380bcba07bdcf3976fb144802a000bd
Adriano Mazziotta <adriano.mazziotta@slu.se>
2018-10-11 15:40:32 +0200

    deleted touch, added comments

--- automate_run.bash
+++ automate_run.bash
@@ -5,9 +5,11 @@ RESULT_DIR=/c/Users/adta0001/data-shell/data-shell/results   #$PWD/results
 #input directory for molecules data
 INPUT_DIR=/c/Users/adta0001/data-shell/data-shell/molecules
 #Do not change here except you know what you are doing!
-
+# remove RESULT_DIR
 rm -rf $RESULT_DIR
+# create directory:
 mkdir $RESULT_DIR
+
 touch $RESULT_DIR/length.txt
 for filename in $INPUT_DIR/*.pdb
     do
